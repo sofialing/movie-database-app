dfb9b2f2226b6c7a5df4f93e1f3307329b832abb
Sofia Ling <sofialing@live.se>
2020-01-25 16:23:59 +0100

    Added components to show movie details

---
+++ src/components/MovieDetails.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import MovieCard from './MovieCard';
+import FeaturedCast from './FeaturedCast';
+
+export default class MovieDetails extends Component {
+
+    render() {
+        return (
+            <div>
+                <MovieCard id={this.props.match.params.movieId} />
+                <FeaturedCast id={this.props.match.params.movieId} />
+            </div>
+        )
+    }
+}
